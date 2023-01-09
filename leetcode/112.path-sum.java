5061fe74ab7fea114346db8990aa96f58e357e92
yuuLab <yuto.yoshida0118@gmail.com>
2023-01-09 22:59:39 +0900

    Practice leetcode

---
+++ leetcode/112.path-sum.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=112 lang=java
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        return hasPathSum(root, targetSum, 0);
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum, int curSum) {
+        if (root == null)
+            return false;
+
+        curSum += root.val;
+        if (root.left == null && root.right == null) {
+            if (curSum == targetSum) {
+                return true;
+            }
+            return false;
+        }
+
+        return hasPathSum(root.left, targetSum, curSum) || hasPathSum(root.right, targetSum, curSum);
+    }
+}
+
+class Solution2 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null)
+            return false;
+
+        if (root.left == null && root.right == null && root.val == targetSum)
+            return true;
+
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+    }
+}
+// @lc code=end
